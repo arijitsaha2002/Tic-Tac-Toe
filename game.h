a4df46e3c1ed073fc9a536314dfb644e2a108e01
Arijit Saha <107265417+sontuarijit@users.noreply.github.com>
2022-08-16 00:27:04 +0530

    Add files via upload

---
+++ game.h
@@ -0,0 +1,152 @@
+# include<iostream>
+# include<stdlib.h>
+# include<time.h>
+using namespace std;
+
+const char empty = '_';
+const char me = 'o';
+const char oponent = 'x';
+
+class Tic_Tac_Tao;
+class Game;
+
+
+
+class Tic_Tac_Tao {
+protected:
+	void dis(char a) {
+		cout << "|" << a;
+		return;
+	}
+	bool is_empty(int i, int j) {
+		if (position[i][j] == empty)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	void make_empty(int i, int j) {
+		if (is_empty(i, j))
+		{
+			cout << "this shell is already empty \n";
+			return;
+		}
+		position[i][j] = empty;
+
+	}
+	char show_position(int i, int j) {
+		return position[i][j];
+	}
+
+public:
+	char position[3][3];
+	Tic_Tac_Tao() {
+		for (int j = 0; j < 3; ++j)
+		{
+			for (int i = 0; i < 3; ++i) { position[i][j] = empty;}
+		}
+	}
+	char win() {
+		for (int i = 0; i < 3; ++i)
+		{
+			if (position[i][0] == position[i][1] && position[i][1] == position[i][2])
+			{
+				if (position[i][0] != empty)
+				{
+					return position[i][0];
+				}
+			}
+		}
+
+		for (int i = 0; i < 3; ++i)
+		{
+			if (position[0][i] == position[1][i] && position[1][i] == position[2][i])
+			{
+				if (position[0][i] != empty)
+				{
+					return position[0][i];
+				}
+
+			}
+		}
+		if (position[0][0] == position[1][1] && position[1][1] == position[2][2])
+		{
+			if (position[0][0] != empty)
+			{
+				return position[0][0];
+			}
+		}
+		if (position[0][2] == position[1][1] && position[1][1] == position[2][0])
+		{
+			if (position[0][2] != empty)
+			{
+				return position[0][2];
+			}
+		}
+		return empty;
+
+	}
+
+	virtual void display() {
+		for (int i = 0; i < 3; ++i)
+		{
+			for (int j = 0; j < 3; ++j) { dis(position[i][j]); }
+			cout << "|" << endl;
+		}
+
+		cout << endl;
+		return;
+	}
+
+	bool input(int i, int j, char a = oponent) {
+		if ( i >= 0 && i < 3 && j >= 0 && j < 3)
+		{
+
+			if (position[i][j] == empty)
+			{
+				position[i][j] = a;
+				return true;
+			}
+		}
+
+		cout << "Invalid input" << endl;
+		return false;
+	}
+	bool no_move() {
+		for (int i = 0; i < 3; ++i)
+		{
+			for (int j = 0; j < 3; ++j)
+			{
+				if (is_empty(i, j))
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	void Win_cheker() {
+		if (no_move() || win() != empty)
+		{
+			if (win() == me)
+			{
+				cout << "!! Game Over !!" << endl;
+			}
+			else if (win() == oponent)
+			{
+				cout << "Congratulation !!" << endl;
+			}
+			else
+			{
+				cout << "! match draw !" << endl;
+			}
+			return;
+		}
+		return;
+	}
+
+
+
+
+};
---
+++ next_move.h
@@ -0,0 +1,115 @@
+# include<iostream>
+# include<stdlib.h>
+# include<time.h>
+# include"game.h"
+using namespace std;
+
+
+
+class Game : public Tic_Tac_Tao {
+
+public:
+	void move() {
+		int a;
+		do {
+			cout << " Enter your position " << endl;
+			cin >> a;
+		} while (!input(a / 10, a % 10));
+	}
+	void first_move() {
+		int i = rand() % 3 + 1;
+		if (i == 2)
+		{
+			input(i-1, i-1,me);
+		}
+		else {
+			int j = rand() % 2 + 1;
+			input(i - 1, 2 * j - 2, me);
+		}
+	}
+	int min_max(bool is_me) {
+		if (win() == oponent)
+		{
+			return -20;
+		}
+		if (win() == me)
+		{
+			return 20;
+		}
+		if (no_move())
+		{
+			return 0;
+		}
+
+		if (is_me)
+		{
+			int best = -500;
+			for (int i = 0; i < 3; ++i)
+			{
+				for (int j = 0; j < 3; ++j)
+				{
+					if (is_empty(i, j))
+					{
+						input(i, j, me);
+						int min_ma = min_max(false);
+						best = max(best, min_ma);
+						make_empty(i, j);
+					}
+				}
+			}
+			return best;
+		}
+
+		else
+		{
+			int best = 500;
+			for (int i = 0; i < 3; ++i)
+			{
+				for (int j = 0; j < 3; ++j)
+				{
+					if (is_empty(i, j))
+					{
+						input(i, j);
+						int min_ma = min_max(true);
+						best = min(best, min_ma);
+						make_empty(i, j);
+					}
+				}
+			}
+			return best;
+		}
+		return 0;
+
+	}
+	bool my_move() {
+		if (win()!=empty)
+		{
+			return false;
+		}
+		int x = -1, y = -1, best_move = -500;
+		for (int i = 0; i < 3; ++i)
+		{
+			for (int j = 0; j < 3; ++j)
+			{
+				if (is_empty(i, j))
+				{
+					input(i, j, me);
+					int score = min_max(false);
+					if (score > best_move)
+					{
+						x = i; y = j;
+						best_move = score;
+					}
+					make_empty(i, j);
+				}
+			}
+		}
+		input(x, y, me);
+		if(win()==me){
+			return false;
+		}
+		return true;
+	}
+
+
+};
